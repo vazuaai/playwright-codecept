06d6f104ecf66134b32940cf048ecbfb3f433a58
Varju Zoli <zoltn.varju@gmail.com>
2023-03-06 12:42:18 +0100

    create first test

--- example_test.js
+++ example_test.js
@@ -1,5 +1,6 @@
-Feature('example');
+Feature('example')
 
-Scenario('test something',  ({ I }) => {
-
-});
+Scenario('Should load example.com website', ({ I }) => {
+  I.amOnPage('https://www.example.com')
+  I.see('Example')
+})
