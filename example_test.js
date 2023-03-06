2ef03592f8988970b26a97abb2e9ae25955dca01
Varju Zoli <zoltn.varju@gmail.com>
2023-03-06 14:30:11 +0100

    create two additional tests

--- example_test.js
+++ example_test.js
@@ -1,6 +1,25 @@
-Feature('example')
+Feature('My test suite')
 
-Scenario('Should load example.com website', ({ I }) => {
+Scenario('Should load example.com website - 1. test', ({ I }) => {
   I.amOnPage('https://www.example.com')
   I.see('Example')
+  I.dontSee('Google')
+  I.seeElement('h1')
+  I.dontSeeElement('#idontexist')
+})
+
+Scenario('Should load example.com website - 2. test', ({ I }) => {
+  I.amOnPage('https://www.example.com')
+  I.see('Example')
+  I.dontSee('Google')
+  I.seeElement('h1')
+  I.dontSeeElement('#idontexist')
+})
+
+Scenario('Should load example.com website - 3. test', ({ I }) => {
+  I.amOnPage('https://www.example.com')
+  I.see('Example')
+  I.dontSee('Google')
+  I.seeElement('h1')
+  I.dontSeeElement('#idontexist')
 })
